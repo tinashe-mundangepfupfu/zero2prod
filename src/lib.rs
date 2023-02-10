293b7426c52c0bf0a950844df11f71f7e33c5fa0
Tinashe Mundangepfupfu <tinashe.mundangepfupfu@deliveryhero.com>
2023-02-10 20:45:44 +0100

    NOREF Add Health Check endpoint and lib

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+pub async fn run() -> std::io::Result<()> {
+    HttpServer::new(|| { App::new()
+        .route("/health_check", web::get().to(health_check))
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
